cc723f483c3aad85653a2c0a5421e015395cb977
Alek Merani <alekmerani@gmail.com>
2020-04-05 02:08:51 -0700

    initial site

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles/index.scss';
+
+const App = () => (
+  <React.Fragment>
+    <div className="App">
+      <a href="https://www.instagram.com/ephemeralcomputations/">@ephemeralcomputations</a>
+    </div>
+  </React.Fragment>
+);
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+if (module.hot) {
+  module.hot.accept();
+}
