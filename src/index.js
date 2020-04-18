efabdf10fc11fa1a9f08aef5d565027e1eefb9a7
Alek Merani <alekmerani@gmail.com>
2020-04-18 13:52:41 -0700

    rename IG

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import './styles/index.scss';
 const App = () => (
   <React.Fragment>
     <div className="App">
-      <a href="https://www.instagram.com/ephemeralcomputations/">@ephemeralcomputations</a>
+      <a href="https://www.instagram.com/phmrlcmpttns/">@phmrlcmpttns</a>
     </div>
   </React.Fragment>
 );
